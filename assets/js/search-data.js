024badffa613e06e219721c306fd86aa806edfba
chaovven <chaovven@users.noreply.github.com>
2025-06-19 23:00:55 +0000

    Deploying to gh-pages from @ yili-law/yili-law.github.io@065987fda0f302028327c2c046106d98b973c6ec 🚀

---
+++ assets/js/search-data.js
@@ -0,0 +1,92 @@
+// get the ninja-keys element
+const ninja = document.querySelector('ninja-keys');
+
+// add the home and posts menu items
+ninja.data = [{
+    id: "nav-about",
+    title: "about",
+    section: "Navigation",
+    handler: () => {
+      window.location.href = "/";
+    },
+  },{id: "nav-blog",
+          title: "blog",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/blog/";
+          },
+        },{id: "nav-projects",
+          title: "projects",
+          description: "A growing collection of your cool projects.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/projects/";
+          },
+        },{id: "nav-cv",
+          title: "cv",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/cv/";
+          },
+        },{id: "post-观察-国际数据治理动态-一-文本和数据挖掘行为-tdm-版权规范的多元路径和演变趋势",
+        
+          title: "观察 | 国际数据治理动态（一）：文本和数据挖掘行为（TDM）版权规范的多元路径和演变趋势",
+        
+        description: "文本和数据挖掘行为（TDM）版权规范的多元路径和演变趋势",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2025/formatting-and-links/";
+          
+        },
+      },{id: "books-the-godfather",
+          title: 'The Godfather',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/the_godfather/";
+            },},{id: "news-i-started-a-new-position-as-a-data-compliance-lawyer-in-chery-automobile",
+          title: 'I started a new position as a data compliance lawyer in Chery Automobile!...',
+          description: "",
+          section: "News",},{
+        id: 'social-email',
+        title: 'email',
+        section: 'Socials',
+        handler: () => {
+          window.open("mailto:%6C%69%77%69%74%68%79%69%6D%65%6C%6F%64%79@%67%6D%61%69%6C.%63%6F%6D", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/yi-li-lawandtech", "_blank");
+        },
+      },{
+      id: 'light-theme',
+      title: 'Change theme to light',
+      description: 'Change the theme of the site to Light',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("light");
+      },
+    },
+    {
+      id: 'dark-theme',
+      title: 'Change theme to dark',
+      description: 'Change the theme of the site to Dark',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("dark");
+      },
+    },
+    {
+      id: 'system-theme',
+      title: 'Use system default theme',
+      description: 'Change the theme of the site to System Default',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("system");
+      },
+    },];
